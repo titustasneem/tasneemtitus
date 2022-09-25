90bc3cb9503919e2b37e8f7f0052ced9b6f09a28
Tasneem Titus <titustasneem5@gmail.com>
2022-09-25 10:05:39 +0200

    added complete js for sticky navBar & for scroll up button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
+    $(window).scroll(function(){
+        // scroll-up button show/hide script start
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+        // scroll-up button show/hide script end
+
+    // slide-up script start
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    // slide-up script end
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
     // typing text animation script start
     var typed = new Typed(".typing", {
         strings: ["Front end developer", "Back end developer", "Web enthusiast", "Coder"],
@@ -13,24 +42,6 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
- 
-       // typing text animation script end
-
-       $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-       
+    // typing text animation script end
 
 });
