1fd34b74078a9b16b55cc7397e8e28009e9ae8da
Tasneem Titus <titustasneem5@gmail.com>
2022-09-25 09:58:40 +0200

    added more basic js for scroll up button

--- src/index.js
+++ src/index.js
@@ -14,5 +14,23 @@ $(document).ready(function(){
         loop: true
     });
  
-       // typing text animation script start
+       // typing text animation script end
+
+       $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+       
+
 });
