f76a0bab4db154a23865ed2128455fa0530458b7
Tasneem Titus <titustasneem5@gmail.com>
2022-09-25 09:32:30 +0200

    added js for typing animation - for home section & for about section

---
+++ src/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // typing text animation script start
+    var typed = new Typed(".typing", {
+        strings: ["Front end developer", "Back end developer", "Web enthusiast", "Coder"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Front end developer", "Back end developer", "Web enthusiast", "Coder"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+ 
+       // typing text animation script start
+});
