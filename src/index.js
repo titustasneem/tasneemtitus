505d09ab7daad870d43e8130f79627c804092993
Tasneem Titus <titustasneem5@gmail.com>
2022-09-26 09:40:26 +0200

    added css, added project description and images

--- src/index.js
+++ src/index.js
@@ -22,18 +22,18 @@ $(document).ready(function(){
 
 // -----------------------------------------------------------------
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+    // $('.navbar .menu li a').click(function(){
+    //     // applying again smooth scroll on menu items click
+    //     $('html').css("scrollBehavior", "smooth");
+    // });
 
 // -----------------------------------------------------------------
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+    // $('.menu-btn').click(function(){
+    //     $('.navbar .menu').toggleClass("active");
+    //     $('.menu-btn i').toggleClass("active");
+    // });
 
 // -----------------------------------------------------------------
 
@@ -78,8 +78,7 @@ $(document).ready(function(){
     }
 });
  // owl carousel script end 
-
-// ----------------------------------------------------------------------------------
+ 
 });
 
 
