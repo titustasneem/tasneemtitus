2cbea16c6df7e91486c2d305b51aab3f545c4498
Tasneem Titus <titustasneem5@gmail.com>
2022-09-26 12:17:31 +0200

    cleaned up js for final submission

--- src/index.js
+++ src/index.js
@@ -1,69 +1,65 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    $(window).scroll(function(){
-        // scroll-up button show/hide script start
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-        // scroll-up button show/hide script end
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-// -----------------------------------------------------------------
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "Front end developer",
+      "Back end developer",
+      "Web enthusiast",
+      "Coder",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    // slide-up script start
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-    // slide-up script end
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Front end developer",
+      "Back end developer",
+      "Web enthusiast",
+      "Coder",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-// -----------------------------------------------------------------
-
-    // typing text animation script start
-    var typed = new Typed(".typing", {
-        strings: ["Front end developer", "Back end developer", "Web enthusiast", "Coder"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Front end developer", "Back end developer", "Web enthusiast", "Coder"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    // typing text animation script end
-
-// -----------------------------------------------------------------
-
- // owl carousel script start 
- $('.carousel').owlCarousel({
+  // owl carousel script
+  $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
     autoplay: true,
     autoplayTimeOut: 2000,
     autoplayHoverPause: true,
     responsive: {
-        0:{
-            items: 1,
-            nav: false
-        },
-        600:{
-            items: 3,
-            nav: false
-        },
-        1000:{
-            items: 5,
-            nav: false
-        }
-    }
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 5,
+        nav: false,
+      },
+    },
+  });
 });
- // owl carousel script end 
- 
-});
-
-
