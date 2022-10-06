0ea8687f3302c227df08ab9958de5527eeabd6f8
Tasneem Titus <titustasneem5@gmail.com>
2022-10-06 16:40:00 +0200

    added js for typed animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "Front end developer",
+      "Back end developer",
+      "Web enthusiast",
+      "Coder",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Front end developer",
+      "Back end developer",
+      "Web enthusiast",
+      "Coder",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+});
 
